1a7e3c0e6aa170364770ecb22a31a98a30c4f4ec
highoctanehosting <support@highoctanemanagement.com>
2023-01-22 16:40:33 -0500

    FInised kata

--- Kata.cs
+++ Kata.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Diagnostics;
 using System.Linq;
@@ -23,7 +22,7 @@ public class Kata
             char currentLetter = stringArray[i];
 
             if (vowels.Contains(currentLetter) && currentLetter != 'y' && currentLetter != ' ')
-                {
+            {
                 Console.WriteLine(char.ToUpper(currentLetter) + " is a vowel.");
                 vowelCount++;
             }
@@ -41,48 +40,4 @@ public class Kata
 
         return vowelCount;
     }
-=======
-﻿using System;
-using System.Diagnostics;
-using System.Linq;
-using System.Text.RegularExpressions;
-
-public class Kata
-{
-    public int GetVowelCount(string str)
-    {
-        string _str = str;
-
-        int vowelCount = 0;
-
-        char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
-
-        char[] stringArray = _str.ToCharArray();
-
-        int i = 0;
-
-        while (i < _str.Length)
-        {
-            char currentLetter = stringArray[i];
-
-            if (vowels.Contains(currentLetter) && currentLetter != 'y' && currentLetter != ' ')
-                {
-                Console.WriteLine(char.ToUpper(currentLetter) + " is a vowel.");
-                vowelCount++;
-            }
-            else
-            {
-                Console.WriteLine(char.ToUpper(currentLetter) + " is NOT a vowel.");
-            }
-
-            i++;
-            Console.WriteLine();
-        }
-
-        Console.WriteLine();
-        Console.WriteLine(_str.ToUpper() + " contains " + vowelCount + " vowels.");
-
-        return vowelCount;
-    }
->>>>>>> fc38c27950dc56bf44f863a03db98f5344b4e01e
 }
