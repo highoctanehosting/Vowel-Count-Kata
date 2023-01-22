fc38c27950dc56bf44f863a03db98f5344b4e01e
High Octane Hosting <73814322+highoctanehosting@users.noreply.github.com>
2023-01-22 15:54:13 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vowel_Count_Kata
+{
+    internal class Program
+    { 
+        static void Main(string[] args)
+        {
+            Kata kata = new Kata();
+
+            kata.GetVowelCount("abracadabra");
+
+            kata.GetVowelCount("The quick brown fox jumps over the lazy dog");
+
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+            while (sw.ElapsedMilliseconds < 20000)
+            {
+
+            }
+            sw.Stop();
+        }
+    }
+}
